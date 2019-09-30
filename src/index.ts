d7256440f5fbbe9f4494311dd08a0808ddb5a024
Jack <jack.levitt@tombola.com>
2019-09-30 10:36:56 +0100

    sort-bar

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import Vue from "vue";
-import MyComponent from "./components/my-component/index.vue";
+import PokemonList from "./components/PokemonList/index.vue";
+import InfoPanel from "./components/InfoPanel/index.vue";
+import sortBar from "./components/sort-bar/index.vue";
+import header_component from "./components/header_component/index.vue";
 import axios from 'axios';
 
 interface IPokemon {
@@ -8,28 +11,37 @@ interface IPokemon {
     types: []
 }
 
+
+
 let v = new Vue({
     el: "#app",
     template: `
-    <div style="display: flex; flex-wrap: wrap; justify-content: center; max-width: 600px; margin: auto">
-        <my-component v-for="pokemon in pokemonData"
-        :name="pokemon.name"
-        :url="pokemon.img"
-        :types="pokemon.type.toString()"
-        :weaknesses="pokemon.weaknesses.toString()"
-        :weight="pokemon.weight"
-        :height="pokemon.height"
-        />       
+    <div>
+        <header_component/>
+        <sortBar/>
+            <div style="display: flex; flex-wrap: wrap; justify-content: center; max-width: 600px; margin: auto">
+                <pokemonList v-for="pokemon in pokemonData"
+                :name="pokemon.name"
+                :url="pokemon.img"
+                :types="pokemon.type.toString()"
+                :weaknesses="pokemon.weaknesses.toString()"
+                :weight="pokemon.weight"
+                :height="pokemon.height"
+                />       
+            </div>
+        <InfoPanel/>
     </div>
     `,
     data() {
         return {
-            names: ["Ryan", "Archer", "Jack"],
             pokemonData:[] as IPokemon[]
         };
     },
     components: {
-        MyComponent
+        PokemonList,
+        sortBar,
+        InfoPanel,
+        header_component
     },
     mounted () {
         var i = axios.get('https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json').then ((response:any) => {
