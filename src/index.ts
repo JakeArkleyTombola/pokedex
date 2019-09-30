86e08a5ebca0805e4b9bc3fc72e704a1f6ed51de
Ryan Walker <ryan.walker@tombola.com>
2019-09-30 16:37:04 +0100

    Pokemon can now be selectivley shown or hidden

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ interface IPokemon {
     weight: string
     height: string
     favourite: boolean
+    shown: boolean
 }
 
 let v = new Vue({
@@ -23,7 +24,7 @@ let v = new Vue({
         <Header/>
         <SortBar/>
             <div v-on:click="changePokemon" style="display: flex; flex-wrap: wrap; justify-content: center; max-width: 600px; margin: auto">
-                <PokemonList v-for="pokemon in currentPokemon"
+                <PokemonList v-for="pokemon in pokemonData" v-if=pokemon.shown
                 :name="pokemon.name"
                 :url="pokemon.img"
                 :types="pokemon.type.toString()"
@@ -37,7 +38,6 @@ let v = new Vue({
     `,
     data: {
         pokemonData:[] as IPokemon[],
-        currentPokemon:[] as IPokemon[],
         infoPanelToggle: false
     },
     components: {
@@ -50,7 +50,7 @@ let v = new Vue({
         var i = axios.get('https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json').then ((response:any) => {
         this.pokemonData = response.data.pokemon
         for (let pokemon of this.pokemonData) {pokemon.favourite = false;}
-        this.currentPokemon = this.pokemonData
+        for (let pokemon of this.pokemonData) {pokemon.shown = true;}
         })
     },
     methods: {
@@ -58,7 +58,7 @@ let v = new Vue({
             this.infoPanelToggle = !this.infoPanelToggle;
         },
         aZSort() {
-            this.currentPokemon.sort(function(a, b) {
+            this.pokemonData.sort(function(a, b) {
                 var x = a.name.toLowerCase();
                 var y = b.name.toLowerCase();
                 if (x < y) {return -1;}
@@ -68,22 +68,22 @@ let v = new Vue({
         },
         zASort() {
             this.aZSort()
-            this.currentPokemon.reverse()
+            this.pokemonData.reverse()
         },
         heightSort() {
-            this.currentPokemon.sort(function(a, b) {
+            this.pokemonData.sort(function(a, b) {
                 var x:number = +a.height.replace(/[^\d.-]/g, '')
                 var y:number = +b.height.replace(/[^\d.-]/g, '')
                 return x - y})
         },
         weightSort() {
-            this.currentPokemon.sort(function(a, b) {
+            this.pokemonData.sort(function(a, b) {
                 var x:number = +a.weight.replace(/[^\d.-]/g, '')
                 var y:number = +b.weight.replace(/[^\d.-]/g, '')
                 return x - y})
         },
         numberSort() {
-            this.currentPokemon.sort(function(a, b) {return +a.id - +b.id})
+            this.pokemonData.sort(function(a, b) {return +a.id - +b.id})
         }
 
     }
